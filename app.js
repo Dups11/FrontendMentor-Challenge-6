480217e4bf9c4e1c91e44abcb0ec73a21e8cd6e4
Dups <dupuis.dav.code@gmail.com>
2021-08-06 10:29:57 +0200

    Create Share function

---
+++ app.js
@@ -0,0 +1,6 @@
+document.getElementById('share').addEventListener('click', function() {
+    document.getElementById('media').classList.toggle('visible');
+    document.getElementById('share').classList.toggle('visible-svg');
+    document.getElementById('social').classList.toggle('visible-social');
+    
+})
